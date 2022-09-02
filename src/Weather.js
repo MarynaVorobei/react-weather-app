1d3d57777e216fce5b30c50cc62b83c455aa2fee
Maryna Vorobei <vorobeymrn@gmail.com>
2022-09-03 00:26:13 +0200

    Added HTML

---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-info" />
+          </div>
+        </div>
+      </form>
+      <h1>Madrid</h1>
+      <ul>
+        <li>Wednesday 09:20</li>
+        <li>Clear sky</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"
+            alt="Clear sky"
+          />
+          28°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 42%</li>
+            <li>Wind: 6 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
