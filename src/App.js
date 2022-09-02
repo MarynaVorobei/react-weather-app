e124a84eebbe87fda3e044d98e07f6d1f3134d2d
Maryna Vorobei <vorobeymrn@gmail.com>
2022-09-02 23:42:05 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://singular-puppy-7d796a.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Maryna Vorobei
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/MarynaVorobei/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://singular-puppy-7d796a.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Maryna Vorobei
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/MarynaVorobei/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
